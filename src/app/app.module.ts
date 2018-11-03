a08984fdb652e2b9bc5d4c137ce0b38a19374f5c
Mohammad sharifi <mmdsharifi@yahoo.com>
2018-11-03 22:51:48 +0330

    init commit :tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataTableComponent } from './data-table/data-table.component';
+import {
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatIconModule
+} from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DataTableComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    LayoutModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/data-table/models/column.ts
@@ -0,0 +1,7 @@
+export interface GridColumn {
+  field: string;
+  title: string;
+  type: string;
+  sortable: boolean;
+  filterable: boolean;
+}
